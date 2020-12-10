36c006e031d4034aa3ef0f75f95857b67b28d46f
camcam2015 <cameron_brooks2015@outlook.com>
2020-12-10 01:31:15 -0500

    demo_prealpha

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
+import ReactDOM from 'react-dom'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Canvas, useFrame } from 'react-three-fiber'
+
+function BoxDemo(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    ReactDOM.render(
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <BoxDemo position={[-1.2, 0, 0]} />
+        <BoxDemo position={[1.2, 0, 0]} />
+      </Canvas>,
+      document.getElementById('root')
+    )
   );
 }
 
